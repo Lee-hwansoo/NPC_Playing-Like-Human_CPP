ae22c9a260d105b91460f85e283e5af2b2e0d90f
Lee-hwansoo <lee_hwansoo@naver.com>
2024-10-22 10:57:51 +0900

    feat : CUDA libtorch 설정

---
+++ thirdparty/torch/configurate_package.cmake
@@ -0,0 +1,25 @@
+project(
+  Torch
+  LANGUAGES CXX
+  VERSION 2.5.0
+  DESCRIPTION "libtorch 2.5.0 + cu124"
+  HOMEPAGE_URL "https://pytorch.org/"
+)
+
+set(${PROJECT_NAME}_INSTALL_PATH ${CMAKE_SOURCE_DIR}/thirdparty/install/${CMAKE_BUILD_TYPE}/${PROJECT_NAME} CACHE STRING "Torch install path")
+set(${PROJECT_NAME}_CMAKE_PATH ${${PROJECT_NAME}_INSTALL_PATH}/share/cmake/Torch/ CACHE STRING "Torch cmake path")
+set(${PROJECT_NAME}_CMAKE_ARGS
+-DCMAKE_INSTALL_PREFIX=${${PROJECT_NAME}_INSTALL_PATH})
+
+set(${PROJECT_NAME}_INCLUDE_PATH ${${PROJECT_NAME}_INSTALL_PATH}/include CACHE STRING "Torch include path")
+set(${PROJECT_NAME}_LIBRARIES Torch CACHE STRING "Torch library path")
+
+set(CMAKE_VS_PLATFORM_TOOLSET_HOST_ARCHITECTURE "x64" CACHE STRING "VS platform toolset architecture")
+set(CMAKE_GENERATOR_TOOLSET "host=x64" CACHE STRING "VS generator toolset")
+
+set(CUDA_TOOLKIT_ROOT_DIR "C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v12.6" CACHE STRING "CUDA Toolkit path")
+set(CMAKE_CUDA_COMPILER "${CUDA_TOOLKIT_ROOT_DIR}/bin/nvcc.exe" CACHE STRING "CUDA compiler path")
+set(CUDA_NVCC_EXECUTABLE "${CUDA_TOOLKIT_ROOT_DIR}/bin/nvcc.exe" CACHE STRING "CUDA exe path")
+set(CUDA_INCLUDE_DIRS "${CUDA_TOOLKIT_ROOT_DIR}/include" CACHE STRING "CUDA include path")
+set(CUDA_CUDART_LIBRARY "${CUDA_TOOLKIT_ROOT_DIR}/lib/x64/cudart.lib" CACHE STRING "CUDA cudart library path")
+set(CMAKE_CUDA_ARCHITECTURES 86 CACHE STRING "CUDA architectures")
