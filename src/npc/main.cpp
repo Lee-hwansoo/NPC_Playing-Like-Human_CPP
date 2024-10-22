7cbbd81f741cf20ccd0625e8cb81cedc62396489
Lee-hwansoo <lee_hwansoo@naver.com>
2024-10-22 17:47:51 +0900

    feat : 상수 추가

--- src/npc/main.cpp
+++ src/npc/main.cpp
@@ -1,6 +1,8 @@
 ﻿#include <iostream>
+#include <npc/constants.hpp>
 
 int main(){
     std::cout << "Hello, World!" << std::endl;
+    std::cout << Constants::Display::WIDTH << std::endl;
     return 0;
 }
