cf4c73db65610983cf87534bf672fd41bfae1c26
Lee-hwansoo <lee_hwansoo@naver.com>
2024-10-22 22:37:10 +0900

    feat : m1 mac, CUDA windows 개발 환경 설정

--- thirdparty/torch/configurate_package.cmake
+++ thirdparty/torch/configurate_package.cmake
@@ -2,7 +2,7 @@ project(
   Torch
   LANGUAGES CXX
   VERSION 2.5.0
-  DESCRIPTION "libtorch 2.5.0 + cu124"
+  DESCRIPTION "libtorch"
   HOMEPAGE_URL "https://pytorch.org/"
 )
 
@@ -11,9 +11,17 @@ set(${PROJECT_NAME}_CMAKE_PATH ${${PROJECT_NAME}_INSTALL_PATH}/share/cmake/Torch
 set(${PROJECT_NAME}_CMAKE_ARGS
 -DCMAKE_INSTALL_PREFIX=${${PROJECT_NAME}_INSTALL_PATH})
 
-set(CUDA_TOOLKIT_ROOT_DIR "C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v11.8" CACHE STRING "CUDA Toolkit path")
-set(CMAKE_CUDA_COMPILER "${CUDA_TOOLKIT_ROOT_DIR}/bin/nvcc.exe" CACHE STRING "CUDA compiler path")
-set(CUDA_NVCC_EXECUTABLE "${CUDA_TOOLKIT_ROOT_DIR}/bin/nvcc.exe" CACHE STRING "CUDA exe path")
-set(CUDA_INCLUDE_DIRS "${CUDA_TOOLKIT_ROOT_DIR}/include" CACHE STRING "CUDA include path")
-set(CUDA_CUDART_LIBRARY "${CUDA_TOOLKIT_ROOT_DIR}/lib/x64/cudart.lib" CACHE STRING "CUDA cudart library path")
-set(CMAKE_CUDA_ARCHITECTURES 86 CACHE STRING "CUDA architectures")
+if(WIN32)
+  # Windows + CUDA 12.4 + CUDA Toolkit 11.8
+  set(CUDA_TOOLKIT_ROOT_DIR "C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v11.8" CACHE STRING "CUDA Toolkit path")
+  set(CMAKE_CUDA_COMPILER "${CUDA_TOOLKIT_ROOT_DIR}/bin/nvcc.exe" CACHE STRING "CUDA compiler path")
+  set(CUDA_NVCC_EXECUTABLE "${CUDA_TOOLKIT_ROOT_DIR}/bin/nvcc.exe" CACHE STRING "CUDA exe path")
+  set(CUDA_INCLUDE_DIRS "${CUDA_TOOLKIT_ROOT_DIR}/include" CACHE STRING "CUDA include path")
+  set(CUDA_CUDART_LIBRARY "${CUDA_TOOLKIT_ROOT_DIR}/lib/x64/cudart.lib" CACHE STRING "CUDA cudart library path")
+  set(CMAKE_CUDA_ARCHITECTURES 86 CACHE STRING "CUDA architectures")
+
+elseif(APPLE)
+  # M1 Mac
+  set(USE_CUDA OFF CACHE BOOL "Use CUDA")
+
+endif()
