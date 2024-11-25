fd283f97b6c1cf88370c6f5ac6c235136283c524
Lee-hwansoo <lee_hwansoo@naver.com>
2024-11-25 16:45:53 +0900

    fix : 보상 수정

--- src/npc/environment.cpp
+++ src/npc/environment.cpp
@@ -214,8 +214,8 @@ real_t TrainEnvironment::calculate_reward(const tensor_t& state, const tensor_t&
     real_t yaw_change = required_action[1].item<real_t>();
 
 	// 보상 컴포넌트들
-	real_t dist_factor = 0.7f;
-	real_t path_factor = 0.3f;
+	real_t dist_factor = 0.8f;
+	real_t path_factor = 0.2f;
 
 	real_t dist_reward = 0.0f;
 	if (normalized_goal_dist > 0.1f) {
@@ -229,7 +229,7 @@ real_t TrainEnvironment::calculate_reward(const tensor_t& state, const tensor_t&
 		real_t k = 1.0f;
 		real_t exp_min = std::exp(-k * 0.1f);
 		real_t exp_max = 1.0f;
-		dist_reward = (0.5 + 0.5f * (std::exp(-(progress) * k) - exp_min) / (exp_max - exp_min)) * dist_factor;
+		dist_reward = (0.5f + 0.5f * (std::exp(-(progress) * k) - exp_min) / (exp_max - exp_min)) * dist_factor;
 	}
 	real_t path_reward = std::exp(-std::abs(normalized_frenet_d) * (15.0f)) * path_factor;
 
